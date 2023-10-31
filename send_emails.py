159d4d296d8cd532601fa8cad7bee004050cf688
Reyner Veliz <reyner@reynerveliz.com>
2023-10-31 11:52:38 -0600

    Add os path for secure password

--- send_emails.py
+++ send_emails.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 def send_email(message):
     host = "smtp.stackmail.com"
     port = 465
 
     username = "reyner@reynerveliz.com"
-    password = "RB1=u^`}?&_@"
+    password = os.getenv("PASSWORD")
 
     receiver = "reyner@reynerveliz.com"
     context = ssl.create_default_context()
